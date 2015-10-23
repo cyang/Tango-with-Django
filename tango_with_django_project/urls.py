947cf4cef3ec5a368f81ebde7a0f8c15e0ba70f8
Christopher Yang <cyang001@citymail.cuny.edu>
2015-10-23 13:20:06 -0400

    Set up Django static media server

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,3 +13,11 @@ urlpatterns = patterns('',
     url(r'^rango/', include('rango.urls'))
 )
 
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
