928ee8c5cc4c14a6b7cef34b453dbbe9a79afbd7
Christopher Yang <cyang001@citymail.cuny.edu>
2015-10-24 00:16:59 -0400

    Add about.html template

---
+++ .bashrc
@@ -0,0 +1,3 @@
+export PYTHONPATH=$PYTHONPATH:/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages
+
+    
--- rango/views.py
+++ rango/views.py
@@ -13,4 +13,6 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page <br> <a href='/rango'>Index</a>")
+    context_dict = {'boldmessage': "I AM THE BOLD ABOUT PAGE"}
+
+    return render(request, 'rango/about.html', context_dict)
